1edec7b1c6e2416487515d3b61de72bf0bc9f65c
Atanas <naskoraist@gmail.com>
2018-06-06 15:22:49 +0300

    03.01.2018 Exam - solutions

---
+++ 08.ExamPreparation/Exam_03_01_2018/DharmaSonarFence.java
@@ -0,0 +1,32 @@
+package ExamPreparation.Exam_03_01_2018;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class DharmaSonarFence {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        for (String input = reader.readLine(); !input.equals("Reprogram"); input = reader.readLine()) {
+            int num = Integer.parseInt(input);
+            for (int i = 31; i >= 1; i--) {
+                if (getBit(num, i) == getBit(num, i - 1)) {
+                    num = flipBit(num, i);
+                    num = flipBit(num, i - 1);
+                    i--;
+                }
+            }
+            System.out.println(Integer.toUnsignedString(num));
+        }
+    }
+
+    private static int flipBit(int number, int position) {
+        int mask = 1 << position;
+        return number ^ mask;
+    }
+
+    private static int getBit(int number, int position) {
+        int mask = number >> position;
+        return mask & 1;
+    }
+}
