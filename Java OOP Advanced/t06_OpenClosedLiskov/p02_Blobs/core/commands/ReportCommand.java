4f057bb099ca5cb2e3d29810e63c55ddaff326f6
Atanas <naskoraist@gmail.com>
2018-07-28 15:18:45 +0300

    Open/Closed and Liskov - Blobs solution

--- Java OOP Advanced/t06_OpenClosedLiskov/p02_Blobs/core/commands/ReportCommand.java
+++ Java OOP Advanced/t06_OpenClosedLiskov/p02_Blobs/core/commands/ReportCommand.java
@@ -4,23 +4,18 @@ import t06_OpenClosedLiskov.p02_Blobs.interfaces.Executable;
 import t06_OpenClosedLiskov.p02_Blobs.models.Blob;
 
 import java.lang.reflect.Field;
-import java.util.Arrays;
 
 public class ReportCommand implements Executable {
 
     @Override
     public void execute(String[] args) {
         Class<Blob> blobClass = Blob.class;
-        Field[] fields = blobClass.getDeclaredFields();
-        Field reportField = Arrays.stream(fields)
-                .peek(f -> f.setAccessible(true))
-                .filter(f -> f.getName().equals("shouldReport"))
-                .findFirst()
-                .orElseThrow();
 
         try {
+            Field reportField = blobClass.getDeclaredField("shouldReport");
+            reportField.setAccessible(true);
             reportField.set(null, true);
-        } catch (IllegalAccessException e) {
+        } catch (IllegalAccessException | NoSuchFieldException e) {
             e.printStackTrace();
         }
     }
